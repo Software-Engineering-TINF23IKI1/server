0c61cd4c12ac9fd7c8bf6dfe46a67ea827ac36e1
DrKeks1010 <jonas.putz@icloud.com>
2024-11-24 12:29:45 +0100

    adds a temporary tcp client script

---
+++ _TMP_tcp_client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST, PORT = "127.0.0.1", 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("Type any message to be sent to the server:")
+
+    while True:
+        msg = input()
+
+        s.sendall(str.encode(msg))
+        print(f"Send [\"{msg}\"] to server!")
+
+        data = s.recv(1024)
+        print(f"Received [\"{data.decode()}\"] from server!")
