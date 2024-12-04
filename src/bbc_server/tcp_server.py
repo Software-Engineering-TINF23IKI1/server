b9aae46673c0d5c99f0b72c275356a9138c10b6d
DrKeks1010 <jonas.putz@icloud.com>
2024-12-04 11:12:10 +0100

    changes tcp_server to user no global variables

--- src/bbc_server/tcp_server.py
+++ src/bbc_server/tcp_server.py
@@ -15,69 +15,79 @@ sys.path.insert(0, os.path.join(repo_root_dir))
 
 from bbc_server import CONFIG
 
-# Variables to store the server and connected clients
-global is_server_running, tcp_server, active_clients
-is_server_running = True
-tcp_server = None
-active_clients = []
+class Tcp_server:
+    def __init__(self, HOST : str, PORT : int):
+        self.tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_server.bind((HOST, PORT))
+        self.tcp_server.listen()
+        self.tcp_server.setblocking(False)
 
-def stop_server(signum, frame):
-    """This method is executed on Ctr-C. 
-    It will shut down the server.
+        self.is_server_running = True
+        self.active_clients = []
+        print(f">>> Server listening on port [{PORT}]")
 
-    Args:
-        signum (_type_): value needed for Ctr-C interception
-        frame (_type_): value needed for Ctr-C interception
-    """
-    print(">>> Stopping server...")
-    global is_server_running
-    is_server_running = False
+        # Add Ctl-C Handler
+        signal(SIGINT, self.stop_server)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(("127.0.0.1", PORT))
+        # Start connection listener
+        self.connection_listener_thread = Thread(target = self.connection_listener)
+        self.connection_listener_thread.start()
 
-    connection_listener_thread.join()
-    tcp_server.close()
-    print(">>> Server sucesfully closed!")
-    exit(1)
+        self.server_listener()
+    
+    def server_listener(self):
+        # Main client read loop
+        while self.is_server_running:
+            for tcp_client, client_address in self.active_clients:
+                try:
+                    # Read data from client
+                    data = tcp_client.recv(1024).strip()
+                    if not data: raise ConnectionResetError()
+                except ConnectionResetError: # Client disconnected
+                    self.active_clients.remove((tcp_client, client_address))
+                    print(f">>> Client [{client_address}] lost connection")
+                    continue
+                except BlockingIOError: # No data available
+                    continue
 
+                # Handle data
+                print(f"[{client_address}] {data.decode()}")
+                # just send back the same data, but upper-cased
+                tcp_client.sendall(data.upper())
 
-def connection_listener():
-    """The listener accepting new connections to the server.
-    """
-    global is_server_running
-    while is_server_running:
-        (tcp_client, client_address) = tcp_server.accept()
-        print(f">>> Handling new client from [{client_address}]")
-        tcp_client.setblocking(False)
-        active_clients.append((tcp_client, client_address))
-        sleep(1)
+            sleep(1)
+
+    def connection_listener(self):
+        """The listener accepting new connections to the server.
+        """
+        while self.is_server_running:
+            try:
+                (tcp_client, client_address) = self.tcp_server.accept()
+                print(f">>> Handling new client from [{client_address}]")
+                tcp_client.setblocking(False)
+                self.active_clients.append((tcp_client, client_address))
+            except BlockingIOError: # No new client available
+                sleep(1)
+
+    def stop_server(self, signum, frame):
+        """This method is executed on Ctr-C. 
+        It will shut down the server.
+
+        Args:
+            signum: value needed for Ctr-C interception
+            frame: value needed for Ctr-C interception
+        """
+        print(">>> Stopping server...")
+        # Sets running server variable to false
+        self.is_server_running = False
+
+        # Stop all running server processes
+        self.connection_listener_thread.join()
+        self.tcp_server.close()
+        print(">>> Server sucesfully closed!")
 
 if __name__ == "__main__":
     HOST = CONFIG.get("server", "HOST")
     PORT = int(CONFIG.get("server", "PORT").strip())
 
-    tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_server.bind((HOST, PORT))
-    tcp_server.listen()
-    signal(SIGINT, stop_server)
-
-    connection_listener_thread = Thread(target = connection_listener)
-    connection_listener_thread.start()
-
-    while is_server_running:
-        for tcp_client, client_address in active_clients:
-            try:
-                data = tcp_client.recv(1024).strip()
-            except ConnectionResetError:
-                active_clients.remove((tcp_client, client_address))
-                print(f">>> Client [{client_address}] lost connection")
-                continue # Error: Eine vorhandene Verbindung wurde vom Remotehost geschlossen
-            except BlockingIOError:
-                continue
-
-            print(f"[{client_address}] {data.decode()}")
-            # just send back the same data, but upper-cased
-            tcp_client.sendall(data.upper())
-
-        sleep(1)
+    Tcp_server(HOST, PORT)
