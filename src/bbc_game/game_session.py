11c43ecf1eae0aa370df3d67f2ebb932110369d3
DrKeks1010 <jonas.putz@icloud.com>
2025-04-15 16:29:01 +0200

    adds a function to add players to the session scope

--- src/bbc_game/game_session.py
+++ src/bbc_game/game_session.py
@@ -3,6 +3,8 @@ from game_code import generate_game_code, unregister_game_code
 
 class Game_session:
     def __init__(self):
+        """Generates a new empty game session with a newly generated game code
+        """
         self.code = generate_game_code()
         self.players = []
         self.state = Game_state.Preperation
@@ -11,5 +13,24 @@ class Game_session:
         self.end_condition = None
         self.shop = None
 
+    def add_player(self, player) -> bool:
+        """Adds a player to the game session scope. When a player enters the game session scope, player packets will
+        be managed by the game session directly. Players can only be added to the scope, while the session is in the
+        preperation state.
+
+        Args:
+            player (_type_): the player to add to the game session scope
+
+        Returns:
+            bool: whether the player could be added to the game session scope
+        """
+        if self.state != Game_state.Preperation:
+            return False
+
+        self.players.append(player)
+        return True
+
     def cleanup(self):
+        """Cleans all resources used by the game session directly
+        """
         unregister_game_code(self.code)
