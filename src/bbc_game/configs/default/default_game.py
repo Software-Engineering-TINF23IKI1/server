ad092a50d2778b71fc7529f7dd23662610673832
DrKeks1010 <jonas.putz@icloud.com>
2025-05-18 16:13:21 +0200

    Changes BaseGame to use GameConfigFactory instead

--- src/bbc_game/configs/default/default_game.py
+++ src/bbc_game/configs/default/default_game.py
@@ -1,5 +1,6 @@
-from bbc_game.game_config import BaseGame
+from bbc_game.game_config import GameConfigFactory
 from bbc_game.configs.default.default_shop import DefaultShop
 from bbc_game.configs.default.default_points import DefaultPointEarning
 
-default_game = BaseGame(shop=DefaultShop, base_currency=0, base_earn_rate=0, base_modifier=1, point_earning=DefaultPointEarning())
+default_game_config_factory = GameConfigFactory(shop=DefaultShop, base_currency=0, base_earn_rate=0, base_modifier=1,
+                                                point_earning=DefaultPointEarning())
--- src/bbc_game/game_config.py
+++ src/bbc_game/game_config.py
@@ -1,10 +1,29 @@
 from bbc_game._typing import ShopType, PointEarning
 from typing import Optional
 
-class BaseGame:
-    def __init__(self, shop: Optional[ShopType] = None, base_currency: float = 0, base_earn_rate: float = 0, base_modifier: float = 1, point_earning: Optional[PointEarning] = None):
-        self._shop = shop
+class GameConfig:
+    def __init__(self, shop: Optional[ShopType] = None, base_currency: float = 0, base_earn_rate: float = 0,
+                 base_modifier: float = 1, point_earning: Optional[PointEarning] = None):
+        self.shop = shop
         self.base_currency = base_currency
         self.base_earn_rate = base_earn_rate
         self.base_modifier = base_modifier
         self.point_earning = point_earning
+
+class GameConfigFactory:
+    def __init__(self, shop: Optional[ShopType] = None, base_currency: float = 0, base_earn_rate: float = 0,
+                 base_modifier: float = 1, point_earning: Optional[PointEarning] = None):
+        self.shop = shop
+        self.base_currency = base_currency
+        self.base_earn_rate = base_earn_rate
+        self.base_modifier = base_modifier
+        self.point_earning = point_earning
+
+    def create_game_config(self) -> GameConfig:
+        """Creates a new GameConfig object using the config provided to the factory
+
+        Returns:
+            GameConfig: A new GameConfig object
+        """
+        return GameConfig(shop=self.shop, base_currency=self.base_currency, base_earn_rate=self.base_earn_rate,
+                          base_modifier=self.base_modifier, point_earning=self.point_earning)
