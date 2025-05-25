f52d8537f79473925c4fd940add2324f53987295
Dralle, Julius <julius.dralle@dlr.de>
2025-05-25 19:30:10 +0200

    Adds top players and endcondition into config

--- src/bbc_game/configs/game_config.py
+++ src/bbc_game/configs/game_config.py
@@ -4,21 +4,25 @@ from bbc_game.point_distribution import PointDistributer, PointDistributerFactor
 
 class GameConfig:
     def __init__(self, point_earning: PointDistributer, shop: Optional[ShopType] = None, base_currency: float = 0,
-                 base_earn_rate: float = 0, base_modifier: float = 1):
+                 base_earn_rate: float = 0, base_modifier: float = 1, base_top_players = 5, base_endcondition = 25):
         self.point_earning = point_earning
         self.shop = shop
-        self.base_currency = base_currency
-        self.base_earn_rate = base_earn_rate
-        self.base_modifier = base_modifier
+        self.base_currency      = base_currency
+        self.base_earn_rate     = base_earn_rate
+        self.base_modifier      = base_modifier
+        self.base_top_players   = base_top_players
+        self.base_endcondition  = base_endcondition
 
 class GameConfigFactory:
     def __init__(self, point_earning_factory: PointDistributerFactory, shop: Optional[ShopType] = None,
-                 base_currency: float = 0, base_earn_rate: float = 0, base_modifier: float = 1):
+                 base_currency: float = 0, base_earn_rate: float = 0, base_modifier: float = 1, base_top_players = 5, base_endcondition = 25):
         self.__shop = shop
         self.__base_currency = base_currency
         self.__base_earn_rate = base_earn_rate
         self.__base_modifier = base_modifier
         self.__point_earning_factory = point_earning_factory
+        self.__base_top_players   = base_top_players
+        self.__base_endcondition  = base_endcondition
 
     def create_game_config(self) -> GameConfig:
         """Creates a new GameConfig object using the config provided to the factory
