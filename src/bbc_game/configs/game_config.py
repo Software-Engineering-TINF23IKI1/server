af8d1fc60568d3d3bd1e988e683c6333eabc190a
Dralle, Julius <julius.dralle@dlr.de>
2025-05-26 15:34:13 +0200

    Adds Base EndConditionFactory to GameConfig

--- src/bbc_game/configs/game_config.py
+++ src/bbc_game/configs/game_config.py
@@ -1,28 +1,29 @@
 from bbc_game._typing import ShopType
 from typing import Optional
 from bbc_game.point_distribution import PointDistributer, PointDistributerFactory
+from bbc_game.end_condition import EndConditionFactory
 
 class GameConfig:
-    def __init__(self, point_earning: PointDistributer, shop: Optional[ShopType] = None, base_currency: float = 0,
-                 base_earn_rate: float = 0, base_modifier: float = 1, base_top_players = 5, base_endcondition = 25):
+    def __init__(self, point_earning: PointDistributer, end_condition_factory: EndConditionFactory, shop: Optional[ShopType] = None, base_currency: float = 0,
+                 base_earn_rate: float = 0, base_modifier: float = 1, base_scoreboard_top_players = 5):
         self.point_earning = point_earning
+        self.base_endcondition = end_condition_factory
         self.shop = shop
         self.base_currency = base_currency
         self.base_earn_rate = base_earn_rate
         self.base_modifier = base_modifier
-        self.base_top_players = base_top_players
-        self.base_endcondition = base_endcondition
+        self.base_scoreboard_top_players = base_scoreboard_top_players
 
 class GameConfigFactory:
-    def __init__(self, point_earning_factory: PointDistributerFactory, shop: Optional[ShopType] = None,
-                 base_currency: float = 0, base_earn_rate: float = 0, base_modifier: float = 1, base_top_players = 5, base_endcondition = 25):
+    def __init__(self, end_condition_factory: EndConditionFactory, point_earning_factory: PointDistributerFactory, shop: Optional[ShopType] = None,
+                 base_currency: float = 0, base_earn_rate: float = 0, base_modifier: float = 1, base_scoreboard_top_players = 5):
         self.__shop = shop
         self.__base_currency = base_currency
         self.__base_earn_rate = base_earn_rate
         self.__base_modifier = base_modifier
         self.__point_earning_factory = point_earning_factory
-        self.__base_top_players = base_top_players
-        self.__base_endcondition = base_endcondition
+        self.__base_scoreboard_top_players = base_scoreboard_top_players
+        self.__base_endcondition = end_condition_factory
 
     def create_game_config(self) -> GameConfig:
         """Creates a new GameConfig object using the config provided to the factory
@@ -30,6 +31,6 @@ class GameConfigFactory:
         Returns:
             GameConfig: A new GameConfig object
         """
-        return GameConfig(point_earning=self.__point_earning_factory.create_point_earner(), shop=self.__shop,
+        return GameConfig(end_condition_factory = self.__base_endcondition, point_earning=self.__point_earning_factory.create_point_earner(), shop=self.__shop,
                           base_currency=self.__base_currency, base_earn_rate=self.__base_earn_rate,
                           base_modifier=self.__base_modifier)
