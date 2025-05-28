41f3ce0a25f030b46813466e51c14322522a4800
Julius <35865838+RubelOS@users.noreply.github.com>
2025-05-28 20:15:15 +0200

    26 implement game loop in game session class (#54)
    * changes game_session.py to follow pep8
    
    * adds basic outline for game loop
    
    * adds reading player packages, adds scoreboard calculation
    
    * Adds Game Loop
    
    * Adds top players and endcondition into config
    
    * Adds End Condition for GameLoop
    
    * adds point distribution to game loop
    
    * adds missing timer update to point distribution
    
    * adds logging for games start
    
    * Removes indentation
    
    * fixes problems with scoreboard getting cleared
    
    * Adds Base EndConditionFactory to GameConfig
    
    * small end condition refactor
    
    * adds base scoreboard to game config
    
    * fixes new name
    
    * fixes endcondition in gameloop, fixes scoreboard in gameloop
    
    * renames endcondition factory
    
    * renames wrongly typed endcondition_factory
    
    * creates endcondition later
    
    * Adds Shop attribute to player and adds attribute handler to game_session
    
    * adds passive earn rate to player currency
    
    * adds missing symbol
    
    * fixes syntax in exception package
    
    * address CR
    
    ---------
    
    Co-authored-by: DrKeks1010 <jonas.putz@icloud.com>
    Co-authored-by: Dralle, Julius <julius.dralle@dlr.de>
    Co-authored-by: Fabian <107812436+fabischw@users.noreply.github.com>

--- src/bbc_game/configs/default/default_game.py
+++ src/bbc_game/configs/default/default_game.py
@@ -1,6 +1,14 @@
 from bbc_game.configs.game_config import GameConfigFactory
 from bbc_game.configs.default.default_shop import DefaultShop
 from bbc_game.configs.default.default_points import default_point_distributer_factory
+from bbc_game.end_condition import PointBasedEndConditionFactory
 
-default_game_config_factory = GameConfigFactory(point_earning_factory=default_point_distributer_factory,
-                                                shop=DefaultShop, base_currency=0, base_earn_rate=0, base_modifier=1)
+
+default_game_config_factory = GameConfigFactory(
+    end_condition_factory=PointBasedEndConditionFactory(250),
+    point_earning_factory=default_point_distributer_factory,
+    shop=DefaultShop,
+    base_currency=0,
+    base_earn_rate=0,
+    base_modifier=1,
+)
