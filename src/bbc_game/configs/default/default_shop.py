f38dee66cbc37eec4ddecab61f4aa150e35b6621
DrKeks1010 <146968909+DrKeks1010@users.noreply.github.com>
2025-06-10 10:34:15 +0200

    improves shop

--- src/bbc_game/configs/default/default_shop.py
+++ src/bbc_game/configs/default/default_shop.py
@@ -15,11 +15,11 @@ class DefaultShop(BaseShop):
                 ClickUpgrade(400, lambda x: x + 1, "Better Clicks VIII"),
                 ClickUpgrade(600, lambda x: x + 1, "Better Clicks IX"),
                 ClickUpgrade(850, lambda x: x + 1, "Better Clicks X"),
-                ClickUpgrade(1200, lambda x: x + 1, "Better Clicks XI"),
-                ClickUpgrade(1500, lambda x: x + 1, "Better Clicks XII"),
-                ClickUpgrade(2000, lambda x: x + 1, "Better Clicks XIII"),
-                ClickUpgrade(2500, lambda x: x + 1, "Better Clicks XIV"),
-                ClickUpgrade(3000, lambda x: x + 1, "Better Clicks XV")
+                ClickUpgrade(1_200, lambda x: x + 1, "Better Clicks XI"),
+                ClickUpgrade(1_500, lambda x: x + 1, "Better Clicks XII"),
+                ClickUpgrade(2_000, lambda x: x + 1, "Better Clicks XIII"),
+                ClickUpgrade(2_500, lambda x: x + 1, "Better Clicks XIV"),
+                ClickUpgrade(3_000, lambda x: x + 1, "Better Clicks XV")
             ],
             "Better Clicks",
             "increases click modifier by 1 per level"
@@ -27,7 +27,11 @@ class DefaultShop(BaseShop):
         TieredUpgrade(
             [
                 ClickUpgrade(500, lambda x: x * 2, "Golden Clicks I"),
-                ClickUpgrade(2000, lambda x: x * 2, "Golden Clicks II")
+                ClickUpgrade(2_000, lambda x: x * 2, "Golden Clicks II"),
+                ClickUpgrade(8_000, lambda x: x * 2, "Golden Clicks III"),
+                ClickUpgrade(25_000, lambda x: x * 2, "Golden Clicks IV"),
+                ClickUpgrade(120_000, lambda x: x * 2, "Golden Clicks V"),
+                ClickUpgrade(600_000, lambda x: x * 2, "Golden Clicks VI"),
             ],
             "Golden Clicks",
             "doubles click modifier for every level purchased"
@@ -39,35 +43,57 @@ class DefaultShop(BaseShop):
                 GainUpgrade(200, lambda x: x + 1, "Passive Income III"),
                 GainUpgrade(400, lambda x: x + 1, "Passive Income IV"),
                 GainUpgrade(700, lambda x: x + 1, "Passive Income V"),
-                GainUpgrade(1000, lambda x: x + 1, "Passive Income VI"),
-                GainUpgrade(1500, lambda x: x + 1, "Passive Income VII"),
-                GainUpgrade(2000, lambda x: x + 1, "Passive Income VIII"),
-                GainUpgrade(2500, lambda x: x + 1, "Passive Income IX"),
-                GainUpgrade(3500, lambda x: x + 1, "Passive Income X")
+                GainUpgrade(1_000, lambda x: x + 1, "Passive Income VI"),
+                GainUpgrade(1_500, lambda x: x + 1, "Passive Income VII"),
+                GainUpgrade(2_000, lambda x: x + 1, "Passive Income VIII"),
+                GainUpgrade(2_500, lambda x: x + 1, "Passive Income IX"),
+                GainUpgrade(3_500, lambda x: x + 1, "Passive Income X")
             ],
             "Passive Income",
             "increases passive gain by 1 per level"
         ),
         TieredUpgrade(
             [
-                GainUpgrade(1000, lambda x: x + 5, "Better Passive Income I"),
-                GainUpgrade(2000, lambda x: x + 5, "Better Passive Income II"),
-                GainUpgrade(4000, lambda x: x + 5, "Better Passive Income III"),
-                GainUpgrade(7000, lambda x: x + 5, "Better Passive Income IV"),
-                GainUpgrade(10000, lambda x: x + 5, "Better Passive Income V"),
-                GainUpgrade(15000, lambda x: x + 5, "Better Passive Income VI"),
-                GainUpgrade(22000, lambda x: x + 5, "Better Passive Income VII"),
-                GainUpgrade(30000, lambda x: x + 5, "Better Passive Income VIII"),
-                GainUpgrade(40000, lambda x: x + 5, "Better Passive Income IX"),
-                GainUpgrade(55000, lambda x: x + 5, "Better Passive Income X")
+                GainUpgrade(1_000, lambda x: x + 5, "Better Passive Income I"),
+                GainUpgrade(2_000, lambda x: x + 5, "Better Passive Income II"),
+                GainUpgrade(4_000, lambda x: x + 5, "Better Passive Income III"),
+                GainUpgrade(7_000, lambda x: x + 5, "Better Passive Income IV"),
+                GainUpgrade(10_000, lambda x: x + 5, "Better Passive Income V"),
+                GainUpgrade(15_000, lambda x: x + 5, "Better Passive Income VI"),
+                GainUpgrade(22_000, lambda x: x + 5, "Better Passive Income VII"),
+                GainUpgrade(30_000, lambda x: x + 5, "Better Passive Income VIII"),
+                GainUpgrade(40_000, lambda x: x + 5, "Better Passive Income IX"),
+                GainUpgrade(55_000, lambda x: x + 5, "Better Passive Income X")
             ],
             "Better Passive Income",
             "increases passive gain by 5 per level"
         ),
+        TieredUpgrade(
+            [
+                GainUpgrade(10_000, lambda x: x + 50, "Insane Passive Income I"),
+                GainUpgrade(20_000, lambda x: x + 50, "Insane Passive Income II"),
+                GainUpgrade(35_000, lambda x: x + 50, "Insane Passive Income III"),
+                GainUpgrade(50_000, lambda x: x + 50, "Insane Passive Income IV"),
+                GainUpgrade(70_000, lambda x: x + 50, "Insane Passive Income V"),
+                GainUpgrade(100_000, lambda x: x + 50, "Insane Passive Income VI"),
+                GainUpgrade(150_000, lambda x: x + 50, "Insane Passive Income VII"),
+                GainUpgrade(225_000, lambda x: x + 50, "Insane Passive Income VIII"),
+                GainUpgrade(350_000, lambda x: x + 50, "Insane Passive Income IX"),
+                GainUpgrade(500_000, lambda x: x + 50, "Insane Passive Income X")
+            ],
+            "Insane Passive Income",
+            "increases passive gain by 50 per level"
+        ),
         GainUpgrade(
-            10000,
+            10_000,
             lambda x: x * 2,
             "Golden Income",
             "Doubles the current passive income"
+        ),
+        GainUpgrade(
+            10_000,
+            lambda x: x * 4,
+            "Ultimate Income",
+            "Quadruples the current passive income"
         )
     ]
